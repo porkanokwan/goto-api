b76acd8f17700243bb8ff63c2f2217fcc332d1c1
porkanokwan <kanokwan.duly@gmail.com>
2022-09-02 20:47:48 +0700

    fixed error middleware

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,3 +1,16 @@
 module.exports = (err, req, res, next) => {
-  res.status(500).json({ message: err.message });
+  if (
+    err.name === "SequelizeValidationError" ||
+    err.name === "SequelizeUniqueConstraintError"
+  ) {
+    err.statusCode = 400;
+    err.message = err.errors[0].message;
+  }
+  if (err.name === "TokenExpiredError") {
+    err.statusCode = 401;
+  }
+  if (err.name === "JsonWebTokenError") {
+    err.statusCode = 401;
+  }
+  res.status(err.statusCode || 500).json({ message: err.message });
 };
